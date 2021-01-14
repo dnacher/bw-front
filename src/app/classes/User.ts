a4a170ad9945c912b5c9897867481fe4d3516a66
DNacher <danielnacher@gmail.com>
2021-01-14 19:09:15 -0300

    modificacion del user

--- src/app/classes/User.ts
+++ src/app/classes/User.ts
@@ -1,7 +1,20 @@
 import {UserType} from "./UserType";
 
 export class User {
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
   userType: UserType=new UserType();
+
+  constructor() {
+    this.firstName= "";
+    this.lastName="";
+    this.email="";
+    this.password="";
+  }
+
+  public getfullName(){
+    return this.firstName + ' ' + this.lastName;
+  }
 }
