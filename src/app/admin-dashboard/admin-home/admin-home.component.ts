ed9ce7668f9f11a3e876fbad8b10ecee6abfd2e8
DNacher <danielnacher@gmail.com>
2021-03-17 08:38:08 -0300

    dashboard comercio

---
+++ src/app/admin-dashboard/admin-home/admin-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-general-menu',
+  templateUrl: './admin-menu.component.html',
+  styleUrls: ['./admin-menu.component.scss']
+})
+export class AdminMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
