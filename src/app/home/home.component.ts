913ff0e8ae7b79cad3057e18eba2d5e7f4058994
dnacher <danielnacher@gmail.com>
2021-04-07 22:13:41 -0300

    cambios en home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,7 @@ import {MetaData} from "../classes/metaData";
 import {ActivatedRoute, Router} from "@angular/router";
 import {DataStockService} from "../services/data.stock.service";
 import {Stock} from "../classes/Stock";
-import {CartLine} from "../classes/CartLine";
 import {AppComponent} from "../app.component";
-import {ToastrService} from "ngx-toastr";
 import Swal from "sweetalert2";
 import {DataProductService} from "../services/data.product.service";
 import {FamilyProduct} from "../classes/FamilyProduct";
@@ -22,25 +20,34 @@ export class HomeComponent implements OnInit {
   public familyProducts: Array<FamilyProduct> = [];
   public stocks: Array<Stock> = [];
   public textSearch: string = '';
-
+  public param: string;
   constructor(private dataStockService: DataStockService,
               private dataProductService: DataProductService,
               private router: Router,
               public appComponent: AppComponent,
-              private toast: ToastrService,
               public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.param = params.get("familyProduct")
+    });
+    if(this.param){
+      this.appComponent.familyProductSelected=this.param;
+    }
     this.loadFamilyProducts();
-    this.appComponent.checkCart();
     this.loadMetaData();
+    this.appComponent.checkCart();
     this.loadProduct();
   }
 
   loadFamilyProducts(){
     this.dataProductService.getFamilyProducts().subscribe(data =>{
       this.familyProducts = data;
-      this.appComponent.familyProductSelected= this.familyProducts[0].name;
+      if(this.param!=null){
+        this.appComponent.familyProductSelected= this.param;
+      }else{
+        this.appComponent.familyProductSelected= this.familyProducts[0].name;
+      }
     });
   }
 
@@ -75,60 +82,8 @@ export class HomeComponent implements OnInit {
                           new MetaData('Ensaladas','../../assets/img/shop03.png'));
   }
 
-  selectProduct(stock: Stock, value: number) {
-    let cartLine = this.createCartLine(stock,value);
-    if(this.appComponent.cart.cartLines.length==0) {
-      cartLine.cartHeader= this.appComponent.cart.cartHeader;
-      this.appComponent.cart.cartLines.push(cartLine);
-    }else{
-      let ind = this.checkCartLine(cartLine.product.id);
-      if(ind!=-1){
-        cartLine.quantity = this.appComponent.cart.cartLines[ind].quantity;
-        this.modifyCartLine(cartLine,value);
-        cartLine.cartHeader= this.appComponent.cart.cartHeader;
-        this.appComponent.cart.cartLines[ind] = cartLine;
-      }else{
-        cartLine.cartHeader= this.appComponent.cart.cartHeader;
-        this.appComponent.cart.cartLines.push(cartLine);
-      }
-    }
-    this.getTotal();
-    this.appComponent.saveCartLocalStorage();
-    this.displayMessage(stock.product.name)
-  }
-
-  getTotal(){
-    let total = 0;
-    this.appComponent.cart.cartLines.forEach(cl =>{
-      total+= cl.subTotal;
-    });
-    this.appComponent.cart.cartHeader.total = total;
-  }
-
-  checkCartLine(id: number){
-    let ind = -1;
-    this.appComponent.cart.cartLines.forEach(cl =>{
-      if(cl.product.id===id){
-        ind = this.appComponent.cart.cartLines.indexOf(cl);
-        return ind;
-      }
-    });
-    return ind;
-  }
-
-  modifyCartLine(cartLine: CartLine, value: number){
-    var y: number = +value;
-    cartLine.quantity+= y;
-    cartLine.subTotal= cartLine.quantity*cartLine.product.price;
-  }
-
-  createCartLine(stock: Stock, value: number): CartLine{
-    let cartLine= new CartLine();
-    cartLine.product = stock.product;
-    cartLine.quantity = +value;
-    cartLine.subTotal = cartLine.quantity * stock.product.price;
-    cartLine.cartHeader = this.appComponent.cart.cartHeader;
-    return cartLine;
+  private selectProduct(stock: Stock, value: number) {
+    this.appComponent.processAddProduct(stock,value);
   }
 
   goToProduct(id: number) {
@@ -162,7 +117,7 @@ export class HomeComponent implements OnInit {
     });
      value.then(val=>{
        if(val.value>0){
-         let value:number = val.value;
+         let value:number = parseInt(val.value);
          this.selectProduct(stock,value);
        }
      });
