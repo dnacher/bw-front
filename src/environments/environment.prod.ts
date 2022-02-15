f95337b2be6613a9e7b57c3260f477be7a108fbd
dnacher <danielnacher@gmail.com>
2022-02-15 09:45:51 -0300

    ultimo commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ship-back.herokuapp.com/public',
-  privateBaseUrl: 'https://ship-back.herokuapp.com/private'
+  // baseUrl: 'https://ship-back.herokuapp.com/public',
+  // privateBaseUrl: 'https://ship-back.herokuapp.com/private'
+  baseUrl: 'http://localhost:8080/public',
+  privateBaseUrl: 'http://localhost:8080/private'
+
 };
